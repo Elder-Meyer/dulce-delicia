0cf2281a70b6984c51560357630db470ed0bda00
Elder-Meyer <meyer.sanchez300@gmail.com>
2023-11-17 23:13:52 -0600

    1.2.0 Edit View (main.jsx)

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { CssBaseline } from '@mui/material';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+    <>
+      <CssBaseline />
+      <App />
+    </>
+);
