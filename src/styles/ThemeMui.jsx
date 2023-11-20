277426adf755a56eea9ab020879727c4ae6fc91a
Elder-Meyer <meyer.sanchez300@gmail.com>
2023-11-19 22:44:34 -0600

    2.3.0 Update Color Theme (material ui)

--- src/styles/ThemeMui.jsx
+++ src/styles/ThemeMui.jsx
@@ -3,37 +3,11 @@ import {createTheme} from "@mui/material";
 export const lightTheme = createTheme({
     palette: {
       mode: 'light',
-      common: {
-        black: 'red', //textos del tooltip
-        white: 'red',
-      },
       primary: {
         main: '#E05297',
         light: '#e785b4',
         dark: '#c70064',
         contrastText: '#E0E2E5', //textos de nav
-      },
-      secondary:{
-        main: 'green',
-        light: 'green',
-        dark: 'green',
-        contrastText: 'green', 
-      },
-      grey:{
-        50: '#0000ff', // fondo del tooltip
-        100: '#0000ff',
-        200: '#0000ff',
-        300: '#0000ff',
-        400: '#0000ff',
-        500: '#0000ff',
-        600: '#0000ff',
-        700: '#0000ff',
-        800: '#0000ff',
-        900: '#0000ff',
-        A100: '#0000ff',
-        A200: '#0000ff',
-        A400: '#0000ff',
-        A700: '#0000ff',
       },
       text: { //color de texto simple
         primary: '#78716c',
@@ -45,12 +19,5 @@ export const lightTheme = createTheme({
         paper: '#ffffff',
         default: '#FCEEF5',
       },
-    },
-  });
-  
-export const darkTheme = createTheme({
-    palette: {
-      mode: 'dark',
-      
     },
   });
