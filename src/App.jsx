66fdf23a89799cb0d6b988e3618f706285744dbd
Elder-Meyer <meyer.sanchez300@gmail.com>
2023-11-19 22:40:12 -0600

    2.2.3 Remove Unnecessary Code (props to change theme dark)

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Router } from "./routes/Router";
 import { ThemeProvider } from "@mui/material";
-import { lightTheme, darkTheme } from "./styles/ThemeMui";
+import { lightTheme } from "./styles/ThemeMui";
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [themeColor, setThemeColor] = useState(lightTheme.palette.primary.main);
-
-  const handleThemeChange = () => {
-    setIsDarkMode((prevMode) => !prevMode);
-  };
-
-  useEffect(() => {
-    setThemeColor(isDarkMode ? darkTheme.palette.primary.main : lightTheme.palette.primary.main);
-  }, [isDarkMode]);
-
   return (
-    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-      <Router isDarkMode={isDarkMode} handleThemeChange={handleThemeChange} />
-      <meta name="theme-color" content={themeColor} />
+    <ThemeProvider theme={lightTheme}>
+      <Router />
     </ThemeProvider>
   );
 }
