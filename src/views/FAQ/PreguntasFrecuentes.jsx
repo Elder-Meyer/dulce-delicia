3733adfa8c88e4aea8b6e52a4feb6411e6130f1b
Elder-Meyer <meyer.sanchez300@gmail.com>
2023-11-22 09:38:00 -0600

    6.1.0 Add FAQ Section

--- src/views/FAQ/PreguntasFrecuentes.jsx
+++ src/views/FAQ/PreguntasFrecuentes.jsx
@@ -4,19 +4,22 @@ import Box from '@mui/material/Box';
 import { AccordionDes } from './AccordionDes';
 import { Bread } from '../../components/customs/Bread';
 import { HomeRounded, LiveHelpRounded } from '@mui/icons-material';
-import { Grid, Paper } from '@mui/material';
+import { Container, Grid, Paper } from '@mui/material';
 
 export const PreguntasFrecuentes = () => {
   return (
-    <Box sx={{bgcolor: "background.default"}}>
+    <Box>
         <Bread migas={[{miga: "INICIO", ruta: "/inicio", icono: <HomeRounded/>},{miga: "PREGUNTAS FRECUENTES", ruta: "/preguntas-frecuentes", icono: <LiveHelpRounded/>}]}/>
 
-        <Paper elevation={0}>
-          <Grid container spacing={1}>
-            <Grid item xs={12} sx={{p:3}}>
-            <Typography variant="h4" color="primary" textAlign='center' my={3}>Preguntas frecuentes</Typography>
-            </Grid>
-            <Grid item xs={12} sx={{p:3}}>
+        <Container maxWidth="lg">
+          <Grid container spacing={3} p={1}>
+            <Grid item xs={12} sm={12} md={4} lg={4}>
+              <Typography variant='h4'  textAlign='cent'>Preguntas Frecuentes</Typography>
+            </Grid> 
+            <Grid item xs={12} sm={12} md={8} lg={8} display={{xs: "none", sm: "none", md:"block", lg: "block"}}></Grid>
+            <Grid item xs={12} sm={12} md={4} lg={4} display={{xs: "none", sm: "none", md:"block", lg: "block"}}></Grid>
+            
+            <Grid item xs={12} sm={12} md={8} lg={8}>
               <Box sx={{padding:"15px"}}>
                 <Box sx={{display: "flex", alignItems: "center", justifyContent: "center"}}>
                   <AccordionDes/>
@@ -25,7 +28,7 @@ export const PreguntasFrecuentes = () => {
             </Grid>
 
           </Grid>
-        </Paper>
+        </Container>
     </Box>
   )
 }
